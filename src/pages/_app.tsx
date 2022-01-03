397c9d26d57085aa497852d1a04cf4095e84b665
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-03 20:36:09 -0300

    feat: 🎉 add framer motion context and alter title page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import { AnimateSharedLayout } from 'framer-motion'
+
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Inicio | Rofran Portfólio</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -18,7 +20,9 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <AnimateSharedLayout>
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
     </>
   )
 }
