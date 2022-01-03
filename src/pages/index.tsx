fcd1d73bc1f080e2efe31d0806022c8e7196345d
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-03 20:43:58 -0300

    feat: 🎉 add homepage template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Main from 'components/Main'
+import HomePage from 'template/HomePage'
+import homePageMock from 'template/HomePage/mock'
 
 export default function Home() {
-  return <Main />
+  return <HomePage {...homePageMock} />
 }
