d9890205d7cac9a0ada174f57f3a81bb2a8db726
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-04 09:01:00 -0300

    add more mock

--- src/components/SectionProjects/mock.ts
+++ src/components/SectionProjects/mock.ts
@@ -3,7 +3,22 @@ import ProjectBoxMock from '../ProjectBox/mock'
 
 const mock: SectionProjectsProps = {
   title: 'Personal Projects',
-  projects: Array.from({ length: 6 }, () => ProjectBoxMock)
+  projects: [
+    ProjectBoxMock,
+    {
+      title: 'Happy',
+      description:
+        'Aplicação web para agendamento de serviços para orphanatos feito na semana Omnistack da Rocketseat',
+      img: {
+        url: '/img/happy.png',
+        alt: 'Happy'
+      }
+    },
+    ProjectBoxMock,
+    ProjectBoxMock,
+    ProjectBoxMock,
+    ProjectBoxMock
+  ]
 }
 
 export default mock
