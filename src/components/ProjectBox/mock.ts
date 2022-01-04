e403352060e1accf6ccddc00b94171eaf061b0e4
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-04 08:58:46 -0300

    add img prop

--- src/components/ProjectBox/mock.ts
+++ src/components/ProjectBox/mock.ts
@@ -3,7 +3,11 @@ import { ProjectBoxProps } from '.'
 const mock: ProjectBoxProps = {
   title: 'Gobarber Ignite',
   description:
-    'Aplicação web para agendamento de serviços para barbearias feito no bootcamp GoStack da Rocketseat'
+    'Aplicação web para agendamento de serviços para barbearias feito no bootcamp GoStack da Rocketseat',
+  img: {
+    url: '/img/ignite-gobarber.png',
+    alt: 'Gobarber Ignite'
+  }
 }
 
 export default mock
