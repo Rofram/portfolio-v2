50edf64e19f545754eead5e52a9e1e4a5af63f6a
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-05 19:13:35 -0300

    feat: implemented custom theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,24 +1,61 @@
-import { createGlobalStyle } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStyleProps = {
+  dark?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStyleProps,
+  DefaultTheme
+> = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme, dark }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    html,
+    body,
+    #__next {
+      height: 100%;
+    }
 
-  body {
-    font-family: "Roboto", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: #121214;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+
+    ${dark
+      ? css`
+          body {
+            background-color: ${theme.colors.darkBg};
+            color: ${theme.colors.white};
+          }
+        `
+      : css`
+          body {
+            background-color: ${theme.colors.lightBg};
+            color: ${theme.colors.black};
+          }
+        `}
+  `}
 `
 
 export default GlobalStyles
