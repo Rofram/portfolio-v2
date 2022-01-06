8c4f30fae1a260e0a1bb7194c7821afd56de8883
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-06 20:54:04 -0300

    feat: remove margin

--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -10,6 +10,6 @@ export const Wrapper = styled.div`
 export const Content = styled.div`
   ${({ theme }) => css`
     flex: 1 0 auto;
-    margin-top: ${theme.spacings.xlarge};
+    /* margin-top: ${theme.spacings.xlarge}; */
   `}
 `
