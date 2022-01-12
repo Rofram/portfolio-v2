2a6657e04fc59d968f4f86539dcafd2f3345377d
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-12 03:48:15 -0300

    add responsivity

--- src/components/SectionProjects/styles.ts
+++ src/components/SectionProjects/styles.ts
@@ -9,7 +9,15 @@ export const Wrapper = styled.section`
   height: 100vh;
 
   > h2 {
+<<<<<<< HEAD
     margin: 3rem 0;
+=======
+    margin-top: 6rem;
+  }
+
+  @media (max-width: 1250px) {
+    height: auto;
+>>>>>>> add responsivity
   }
 
   @media (max-width: 1050px) {
@@ -20,15 +28,19 @@ export const Wrapper = styled.section`
 export const Content = styled.div`
   margin-top: 6rem;
   display: grid;
+  margin-top: 5rem;
+  height: 60rem;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 8rem;
   place-items: center;
 
   @media (max-width: 1250px) {
     grid-template-columns: repeat(2, 1fr);
+    height: auto;
   }
 
   @media (max-width: 800px) {
     grid-template-columns: 1fr;
+    height: auto;
   }
 `
