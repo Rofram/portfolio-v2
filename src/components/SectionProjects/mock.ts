bf442d006058504d88604829f99d705937a7c177
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-15 22:40:27 -0300

    add required props

--- src/components/SectionProjects/mock.ts
+++ src/components/SectionProjects/mock.ts
@@ -8,7 +8,8 @@ const mock: SectionProjectsProps = {
     {
       title: 'Happy',
       description:
-        'Aplicação web para agendamento de serviços para orphanatos feito na semana Omnistack da Rocketseat',
+        'Aplicação web para agendamento de serviços para orfanatos feito na semana Omnistack da Rocketseat',
+      link: '#',
       img: {
         url: '/img/happy.png',
         alt: 'Happy'
@@ -18,36 +19,40 @@ const mock: SectionProjectsProps = {
       title: 'Wongames',
       description:
         'E-commerce de games desenvolvido no curso React Avançado do Willian Justen',
+      link: '#',
       img: {
-        url: '/img/happy.png',
-        alt: 'Happy'
+        url: '/img/wongames.svg',
+        alt: 'Wongames'
       }
     },
     {
-      title: 'Happy',
+      title: 'Move-it',
       description:
-        'Aplicação web para agendamento de serviços para orphanatos feito na semana Omnistack da Rocketseat',
+        'O Move.it é um app que usa a técnica de Pomodoro, ao terminar os 25 minutos do contador são oferecidos desafios que trabalham o corpo e a visão.',
+      link: 'https://moveit-nextjs-bice.vercel.app/',
       img: {
-        url: '/img/happy.png',
-        alt: 'Happy'
+        url: '/img/moveit.svg',
+        alt: 'Move-it'
       }
     },
     {
-      title: 'Happy',
+      title: 'Ig.news',
       description:
-        'Aplicação web para agendamento de serviços para orphanatos feito na semana Omnistack da Rocketseat',
+        'O projeto ig.news é um blog onde os usuários podem ter acesso ao conteúdo de cada postagem de acordo com o status de sua assinatura.',
+      link: '#',
       img: {
-        url: '/img/happy.png',
-        alt: 'Happy'
+        url: '/img/igNews.svg',
+        alt: 'Ig.news'
       }
     },
     {
-      title: 'Happy',
+      title: 'Letmeask',
       description:
-        'Aplicação web para agendamento de serviços para orphanatos feito na semana Omnistack da Rocketseat',
+        'Já pensou que em lives as pessoas recebem muitas perguntas, principalmente quando se ensina alguma coisa, ou apresenta algum produto/serviço?',
+      link: '#',
       img: {
-        url: '/img/happy.png',
-        alt: 'Happy'
+        url: '/img/Letmeask.svg',
+        alt: 'Letmeask'
       }
     }
   ]
